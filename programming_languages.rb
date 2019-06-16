2ac5de0554975e11dbab8abbd05b7ddbc7c1e5e2
JahazielGuzman <jahaziel.guzman93@gmail.com>
2019-06-15 21:54:24 -0400

    programming_languages complete

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,17 @@
+require 'pry'
+
 def reformat_languages(languages)
   # your code here
+  lang_prime = {}
+  languages.each do |style, langs|
+  	langs.each do |lang, val|
+  		# check if we initiated the languages in hash
+  		unless lang_prime.key? lang
+	  		lang_prime[lang] = val
+	  		lang_prime[lang][:style] = []
+	  	end
+  		lang_prime[lang][:style] << style
+  	end
+  end
+  lang_prime
 end
